d68c1577d37e6f166d4c528e919e703890b5b051
prandogabriel <gprando55@gmail.com>
2024-06-03 21:37:29 -0300

    feat: add final

--- src/functions/shortener/handler.ts
+++ src/functions/shortener/handler.ts
@@ -1,12 +1,28 @@
 import type { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import { saveUrl } from "src/repositories/url-repository";
 
 export const main: APIGatewayProxyHandlerV2 = async (event) => {
   const body = JSON.parse(event.body);
+  const url = body.url;
+
+  if (!url) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: `url is required`
+      })
+    }
+  }
+
+  const shortId = Math.random().toString(36).substring(7);
+
+  await saveUrl(url, shortId);
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `received: ${body.url}`
+      shortId: shortId,
+      retrieveUrl: `${process.env.RETRIEVE_URL}/${shortId}`
     })
   }
 };
